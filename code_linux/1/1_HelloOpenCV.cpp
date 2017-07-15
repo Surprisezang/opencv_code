7ad073d989476c641a8d6c98e514ec567a2ed15a
hongfazhizun <hongfazhizun@gmail.com>
2017-07-15 16:37:33 +0800

    第一章代码

--- code_linux/1/1_HelloOpenCV.cpp
+++ code_linux/1/1_HelloOpenCV.cpp
@@ -15,7 +15,7 @@ using namespace cv; //包含cv命名空间
 int main()
 {
 	// 【1】读入一张图片
-	Mat img=imread("1.jpg");
+	Mat img=imread("../1/1.jpg");
 	// 【2】在窗口中显示载入的图片
 	imshow("【载入的图片】",img);
 	// 【3】等待6000 ms后窗口自动关闭
